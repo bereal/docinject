2e8c68bcfeb554ab3652271b4b7f973470f6dcc7
bereal <alexander.berezhnoy@gmail.com>
2012-07-25 22:29:12 +0300

    First context implementation

---
+++ docinject/context.py
@@ -0,0 +1,93 @@
+import inspect
+
+def parse_doc(s):
+    result = {}
+    
+    for line in (l.strip() for l in s.splitlines()):
+        if not line: continue
+        
+        words = iter(line.split())
+        first = words.next()
+        if first.startswith('@'):
+            result.setdefault(first, []).extend(words)
+
+    return result
+
+class RoleOverriding(Exception): pass
+class RoleNotFound(Exception): pass
+class DependencyLoop(Exception): pass
+
+class Context(object):
+    CREATING = object()
+    NOT_FOUND = object()
+    
+    def __init__(self):
+        self._roles = {}
+        self._instances = {}
+
+    def __check_free(self, name):
+        if name in self._roles:            
+            raise RoleOverriding(name)
+
+    
+    def __inject_one(self, item, role, dependencies):
+        self.__check_free(role)
+        self._roles[role] = (item, dependencies)
+        
+    def register_instance(self, name, inst):
+        self.__check_free(name)
+        self._instances[name] = inst
+        self._roles[name] = inst
+        
+    def inject_item(self, item, role=None, dependencies=(), enforce=True):
+        if role:
+            self.__inject_one(item, role, dependencies)
+            return True
+        
+        doc = getattr(item, '__doc__', '')
+        attrs = parse_doc(doc)
+        roles = attrs.get('@role')
+        if not roles:
+            if enforce:
+                raise RoleNotFound(item)
+            return False
+
+        dependencies = attrs.get('@requires', ())
+        for r in roles:
+            self.__inject_one(item, r, dependencies)
+
+        for r in attrs.get('@role-inst', ()):
+            self.register_isntance(r, item)
+
+    def inject_module(self, module):
+        modname = module.__name__
+        for name, item in inspect.getmembers(module):
+            try:
+                if item.__module__ != modname:
+                    continue
+            except AttributeError:
+                continue
+
+            self.inject_item(item, enforce=False)
+
+    def get_instance(self, role):
+        if role not in self._roles:
+            raise RoleNotFound(role)
+        
+        existing = self._instances.get(role, self.NOT_FOUND)
+        if existing is self.CREATING:
+            raise DependencyLoop()
+
+        if existing is not self.NOT_FOUND:
+            return existing
+
+        self._instances[role] = self.CREATING
+        constr, dependencies = self._roles[role]
+        try:
+            dep_inst = [self.get_instance(dep) for dep in dependencies]
+            inst = constr(*dep_inst)
+            self._instances[role] = inst
+            return inst
+        except:
+            del self._instances[role]
+            raise
